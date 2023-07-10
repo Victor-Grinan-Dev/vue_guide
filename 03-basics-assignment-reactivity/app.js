220395b4d20776a0c6a20712ad46cfd319fcfbe7
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-07-10 15:35:03 +0300

    resourses added

--- 03-basics-assignment-reactivity/app.js
+++ 03-basics-assignment-reactivity/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     return {
       result: 0,
       status: "🤖 Hello, lets play",
+      activeBtns: true,
     };
   },
   computed: {
@@ -15,19 +16,31 @@ const app = Vue.createApp({
       if (this.result > 0 && this.result < 37) {
         this.status = "🤖 Not there yet";
       } else if (this.result > 37) {
+        this.activeBtns = false;
         this.status = "🤖 number is passed already, you loose!!";
         const data = this;
         setTimeout(() => {
           data.result = 0;
+          data.status = "🤖 lets play again!";
+          data.activeBtns = true;
         }, 5000);
       } else if (this.result === 37) {
         this.status = "🤖 congrats you found the secret number!";
+        this.activeBtns = false;
+        const data = this;
+        setTimeout(() => {
+          data.result = 0;
+          data.status = "🤖 lets play again!";
+          data.activeBtns = true;
+        }, 5000);
       }
     },
   },
   methods: {
     add(number) {
-      this.result = this.result + number;
+      if (this.activeBtns) {
+        this.result = this.result + number;
+      }
     },
   },
 }).mount("#assignment");
