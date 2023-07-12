3471c504516e22c881312f5735dc162bde8142aa
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-07-12 13:17:08 +0300

    add if directive

--- 201-lists-conditional/app.js
+++ 201-lists-conditional/app.js
@@ -1,7 +1,21 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goals: [],
+      newGoal: "",
+    };
+  },
+  // watch: {
+  //   newGoal(val) {
+  //     console.log(val);
+  //   },
+  // },
+  methods: {
+    addGoal() {
+      this.goals.push(this.newGoal);
+      this.newGoal = "";
+    },
   },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
