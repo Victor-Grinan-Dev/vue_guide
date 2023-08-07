38450a11018054e102b830218ecfa85315f11c29
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-07 19:39:27 +0300

    slots

---
+++ 401-advance-comp/src/components/BaseCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="base-card">
+    <header class="base-card__header">
+      <slot name="header"></slot>
+    </header>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [],
+};
+</script>
+
+<style scoped>
+.base-card {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+.base-card__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
--- 401-advance-comp/src/main.js
+++ 401-advance-comp/src/main.js
@@ -6,12 +6,14 @@ import App from "./App.vue";
 // import BadgeList from "./components/BadgeList.vue";
 // import UserInfo from "./components/UserInfo.vue";
 import BaseBadge from "./components/BaseBadge.vue";
-
+import BaseCard from "./components/BaseCard.vue";
 const app = createApp(App);
 
 // app.component('the-header', TheHeader);
 // app.component("badge-list", BadgeList);
 // app.component("user-info", UserInfo);
+
 app.component("base-badge", BaseBadge);
+app.component("base-card", BaseCard);
 
 app.mount("#app");
