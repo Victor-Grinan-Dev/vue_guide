7bf00a6d003a6ebbca6a5d5c26b50a37979cfcc3
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-07 15:50:00 +0300

    register components locally

--- 401-advance-comp/src/App.vue
+++ 401-advance-comp/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
+    <!-- <TheHeader /> -->
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -11,13 +12,24 @@
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
 export default {
+  components: {
+    // "the-header": TheHeader, //standard way to register a component locally
+    // TheHeader: TheHeader, //this allows you to use self closing tag <TheHeader />
+    TheHeader, //even shorter hand syntax
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
