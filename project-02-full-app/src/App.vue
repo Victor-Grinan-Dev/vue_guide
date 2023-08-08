742f7cf61acd68656a73ab094a6e201105a93a79
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-08 13:13:26 +0300

    initial commit for project

---
+++ project-02-full-app/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+// import TheHeader from "./components/TheHeader.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+// import ActiveGoals from "./components/ActiveGoals.vue";
+// import ManageGoals from "./components/ManageGoals.vue";
+
+export default {
+  components: {
+    // "the-header": TheHeader, //standard way to register a component locally
+    // TheHeader: TheHeader, //this allows you to use self closing tag <TheHeader />
+    // TheHeader, //even shorter hand syntax
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    // ActiveGoals,
+    // ManageGoals,
+  },
+  data() {
+    return {
+      storeResourses: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation.",
+          link: "https://vuejs.org/",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "The original Google engine.",
+          link: "https://google.com/",
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ project-02-full-app/src/components/ActiveGoals.vue
@@ -0,0 +1,7 @@
+<template>
+  <h3>Active Goals tab</h3>
+</template>
+
+<script>
+export default {};
+</script>
