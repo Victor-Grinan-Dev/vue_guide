006a813d7a0eb4361c845fc2ed88bc43b15bffb1
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-09 14:38:34 +0300

    data empty msg

--- project-03-survey-app/src/components/survey/UserExperiences.vue
+++ project-03-survey-app/src/components/survey/UserExperiences.vue
@@ -8,7 +8,10 @@
         >
       </div>
       <p v-if="isLoading">Loading...</p>
-      <ul v-if="!isLoading">
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        No stored data yet, please enter some data to start.
+      </p>
+      <ul v-else-if="!isLoading && results && results.length !== 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
