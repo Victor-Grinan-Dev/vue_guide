d963c6483bf8b8898bb633d4897457d3f96b98d0
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-13 17:18:36 +0300

    mutations

---
+++ 601-vuex/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="subsOne">substract 1</button>
+    <button @click="addOne">add 1</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from "./components/BaseContainer.vue";
+import TheCounter from "./components/TheCounter.vue";
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+  },
+
+  methods: {
+    addOne() {
+      this.$store.commit("increment");
+    },
+    subsOne() {
+      this.$store.commit("decrement");
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
