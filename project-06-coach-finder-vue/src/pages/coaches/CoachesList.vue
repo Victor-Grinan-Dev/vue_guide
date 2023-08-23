d53dc2b028c3f63659efd7d9f43d2a6e92ef4c3d
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-23 20:29:22 +0300

    styles of cards

--- project-06-coach-finder-vue/src/pages/coaches/CoachesList.vue
+++ project-06-coach-finder-vue/src/pages/coaches/CoachesList.vue
@@ -1,24 +1,26 @@
 <template>
   <section>Filter</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filterdCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      >
-        {{ coach.firstName }}
-      </coach-item>
-    </ul>
-    <h3 v-else>no coaches found</h3>
+    <base-card>
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as Coach</router-link>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filterdCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        >
+          {{ coach.firstName }}
+        </coach-item>
+      </ul>
+      <h3 v-else>no coaches found</h3></base-card
+    >
   </section>
 </template>
 <script>
