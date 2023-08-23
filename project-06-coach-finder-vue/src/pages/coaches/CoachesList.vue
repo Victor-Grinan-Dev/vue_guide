24eedf42bf0c102fb13769e5403bd8c1c212d105
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-23 19:33:55 +0300

    add coaches data

--- project-06-coach-finder-vue/src/pages/coaches/CoachesList.vue
+++ project-06-coach-finder-vue/src/pages/coaches/CoachesList.vue
@@ -1,1 +1,32 @@
-<template>coaches list</template>
+<template>
+  <section>Filter</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <li
+        v-for="coach in filterdCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach - lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  computed: {
+    filterdCoaches() {
+      return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
+</script>
