61f01a2f710646708de5c8fc318b505f0b3abac7
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-24 14:17:05 +0300

    filter coaches

---
+++ project-06-coach-finder-vue/src/components/coaches/CoachFilter.vue
@@ -0,0 +1,66 @@
+<template>
+  <base-card
+    ><h2>Find Your Coach</h2>
+    <span class="filter-option">
+      <input
+        type="checkbox"
+        name="checkbox"
+        id="frontend"
+        checked
+        @change="setFilter"
+      />
+      <label for="frontend">Frontend</label>
+    </span>
+    <span class="filter-option">
+      <input
+        type="checkbox"
+        name="checkbox"
+        id="backend"
+        checked
+        @change="setFilter"
+      />
+      <label for="backend">backend</label>
+    </span>
+    <span class="filter-option">
+      <input
+        type="checkbox"
+        name="checkbox"
+        id="career"
+        checked
+        @change="setFilter"
+      />
+      <label for="career">career</label>
+    </span>
+  </base-card>
+</template>
+<script>
+export default {
+  emits: ["change-filters"],
+  data() {
+    return {
+      filters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+  methods: {
+    setFilter(event) {
+      const inputId = event.target.id;
+      const isActive = event.target.checked;
+
+      const updatedFilters = {
+        ...this.filters,
+        [inputId]: isActive,
+      };
+
+      this.filters = updatedFilters;
+      this.$emit("change-filters", updatedFilters);
+    },
+  },
+};
+</script>
+<style>
+@import "../../styles/coach-filter.css";
+</style>
--- project-06-coach-finder-vue/src/pages/coaches/CoachesList.vue
+++ project-06-coach-finder-vue/src/pages/coaches/CoachesList.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>Filter</section>
+  <coach-filter @change-filters="setFilters"></coach-filter>
   <section>
     <base-card>
       <div class="controls">
@@ -25,16 +25,45 @@
 </template>
 <script>
 import CoachItem from "../../components/coaches/CoachItem.vue";
+import CoachFilter from "../../components/coaches/CoachFilter.vue";
 export default {
   components: {
     CoachItem,
+    CoachFilter,
+  },
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
   },
   computed: {
     filterdCoaches() {
-      return this.$store.getters["coaches/coaches"];
+      const coaches = this.$store.getters["coaches/coaches"];
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes("frontend")) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes("backend")) {
+          return true;
+        }
+        if (this.activeFilters.career && coach.areas.includes("career")) {
+          return true;
+        }
+        return false;
+      });
     },
     hasCoaches() {
       return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      console.log(updatedFilters);
+      this.activeFilters = updatedFilters;
     },
   },
 };
