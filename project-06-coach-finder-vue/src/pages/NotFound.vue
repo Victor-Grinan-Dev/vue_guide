916abc5e877c12db53255eb56ba04e5731acfb23
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-31 10:14:35 +0300

    ready to add auth

--- project-06-coach-finder-vue/src/pages/NotFound.vue
+++ project-06-coach-finder-vue/src/pages/NotFound.vue
@@ -1,1 +1,10 @@
-<template>not found</template>
+<template>
+  <base-card>
+    <h2>Page not found</h2>
+    <p>
+      This page you are trying to access could not be found. Check out all our
+      <router-link to="/coaches">coaches</router-link>.
+    </p>
+  </base-card>
+</template>
+<style scoped></style>
