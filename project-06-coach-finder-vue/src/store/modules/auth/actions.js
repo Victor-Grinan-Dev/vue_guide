15186c759a9b7e07907d89cb4596e70c54450df7
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-14 10:11:49 +0300

    preserve login

--- project-06-coach-finder-vue/src/store/modules/auth/actions.js
+++ project-06-coach-finder-vue/src/store/modules/auth/actions.js
@@ -5,64 +5,10 @@ import { AUTH_SIGNIN_ENDPOINT } from "../../../ENV/fakeENV";
 export default {
   async signin(context, payload) {
     context.dispatch("auth", { ...payload, mode: "signin" });
-
-    // const response = await fetch(
-    //   `${AUTH_SIGNIN_ENDPOINT}AIzaSyDQQW0nJeyW47JZg9tF8SCqPEHRPM-T5k4`, //${context.rootGetters.token}
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       email: payload.email,
-    //       password: payload.password,
-    //       returnSecureToken: true,
-    //     }),
-    //   }
-    // );
-
-    // const responseData = await response.json();
-    // if (!response.ok) {
-    //   const error = new Error(response.message || "Failed to authenticate");
-    //   throw error;
-    // }
-    // context.commit("setUser", {
-    //   token: responseData.idToken,
-    //   userId: responseData.localId,
-    //   expiresIn: responseData.expiresIn,
-    // });
   },
 
   async signup(context, payload) {
     context.dispatch("auth", { ...payload, mode: "signup" });
-    // const response = await fetch(
-    //   `${AUTH_SIGNUP_ENDPOINT}AIzaSyDQQW0nJeyW47JZg9tF8SCqPEHRPM-T5k4`,
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       email: payload.email,
-    //       password: payload.password,
-    //       returnSecureToken: true,
-    //     }),
-    //   }
-    // );
-
-    // const responseData = await response.json();
-    // if (!response.ok) {
-    //   const error = new Error(response.message || "Failed to authenticate");
-    //   throw error;
-    // }
-
-    // context.commit("setUser", {
-    //   token: responseData.idToken,
-    //   userId: responseData.localId,
-    //   expiresIn: responseData.expiresIn,
-    // });
-  },
-
-  logout(context) {
-    context.commit("setUser", {
-      token: null,
-      userId: null,
-      expiresIn: null,
-    });
   },
 
 async auth(context, payload){
@@ -88,10 +34,34 @@ const response = await fetch(
       const error = new Error(response.message || "Failed to authenticate");
       throw error;
     }
+
+    localStorage.setItem('coachesToken', responseData.idToken);
+    localStorage.setItem('coachesUserId', responseData.localId);
+
     context.commit("setUser", {
       token: responseData.idToken,
       userId: responseData.localId,
       expiresIn: responseData.expiresIn,
     });
-}
+  },
+  tryLogin(context){
+    const token = localStorage.getItem('coachesToken');
+    const userId = localStorage.getItem('coachesUserId');
+
+    if (token && userId){
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        expiresIn: null,
+      })
+    }
+
+  },
+  logout(context) {
+    context.commit("setUser", {
+      token: null,
+      userId: null,
+      expiresIn: null,
+    });
+  },
 };
